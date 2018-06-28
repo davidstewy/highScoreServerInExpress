a4b1fe04681519273f3e21c4c18de472d36d3e19
David Stewart <davidstewy@gmail.com>
2018-06-28 16:26:01 -0400

    added router, error handling, took out content type

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const jsonBody = require("body/JSON");
 const app = express();
 const hostname = null;
 const port = 3000;
 app.use(express.json());
+let router = express.Router();
+app.use('/api', router);
+
 
 let scores = [{
     name: "Edwin",
@@ -13,9 +15,9 @@ let scores = [{
     score: 39
 }];
 
-app.get('/scores', function (req, res) {
-    res.statusCode = 200;
-    res.set('Content-Type', 'application/javascript');
+router.get('/scores', function (req, res) {
+    res.status(201);
+    // res.set('Content-Type', 'application/json');
     console.log(res);
     res.send(scores);
 });
@@ -25,24 +27,26 @@ app.get('/scores', function (req, res) {
 //     res.send("Error Not Found");
 // });
 
-app.post('/scores', function (req, res) {
-    res.statusCode = 201;
-    
-    
-        scores.push(req.body)
-        scores.sort((a, b) => b.score - a.score)
-        scores = scores.slice(0, 3)
-        res.set("Content-Type", "application/json")
-        res.send(scores)
-        
-    
+router.post('/scores', function (req, res) {
+    res.status(201);
+    scores.push(req.body)
+    scores.sort((a, b) => b.score - a.score)
+    scores = scores.slice(0, 3)
+    // res.set("Content-Type", "application/json")
+    res.send(scores)
+
+})
+
+app.use(function(req, res, next){
+    res.status(404).json({
+        error: 'Not Found'
+    })
+    // if (req.accepts('json')){
+    //     res.send({error: 'Not Found'});
+    //     return;
+    // }
 })
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
-
-
-
